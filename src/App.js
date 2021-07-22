c049f091801c9282ca10fe4aac4e10cdf485b535
Shaun <Shaun@Shauns-MacBook-Pro.local>
2021-07-22 15:30:52 -0600

    add sidebar component to App and import it add some styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Lets build a discord clone</h1>
+      <Sidebar />
     </div>
   );
 }
