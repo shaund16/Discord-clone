65dae9e9b2bf59b324f0097d88dcbe084aaddcea
Shaun <Shaun@Shauns-MacBook-Pro.local>
2021-07-22 15:52:44 -0600

    add styles to sidebar and add channels

---
+++ src/SidebarChannel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SidebarChannel.css';
+
+const SidebarChannel = ({ id, channel }) => {
+  return (
+    <div className='sidebarChannel'>
+      <h4><span className='sidebarChannel__hash'>#</span>React</h4>
+    </div>
+  )
+}
+
+export default SidebarChannel
