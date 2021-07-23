46393bf97bc6a99980aec64db2b034eabf98948e
Shaun <Shaun@Shauns-MacBook-Pro.local>
2021-07-22 20:05:02 -0600

    add some styles and create the functionality

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Sidebar.css';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
@@ -10,8 +10,35 @@ import { Avatar } from '@material-ui/core';
 import MicIcon from '@material-ui/icons/Mic';
 import HeadsetIcon from '@material-ui/icons/Headset';
 import SettingsIcon from '@material-ui/icons/Settings';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
+import db, { auth } from './firebase';
 
 const Sidebar = () => {
+  const user = useSelector(selectUser);
+  const [channels, setChannels] = useState([]);
+
+  useEffect(() => {
+    db.collection('channels').onSnapshot((snapshot) =>
+      setChannels(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          channel: doc.data(),
+        }))
+      )
+    );
+  }, []);
+
+  const handleAddChannel = () => {
+    const channelName = prompt('Enter a new channel name');
+
+    if (channelName) {
+      db.collection('channels').add({
+        channelName: channelName,
+      });
+    }
+  };
+
   return (
     <div className='sidebar'>
       <div className='sidebar__top'>
@@ -22,45 +49,47 @@ const Sidebar = () => {
       <div className='sidebar__channels'>
         <div className='sidebar__channelsHeader'>
           <div className='sidebar__header'>
-         <ExpandMoreIcon />
-         <h4>Text Channels</h4>   
+            <ExpandMoreIcon />
+            <h4>Text Channels</h4>
           </div>
 
-          <AddIcon className='sidebar__addChannel' />
+          <AddIcon onClick={handleAddChannel} className='sidebar__addChannel' />
         </div>
-      <div className="sidebar__channelsList">
-        <SidebarChannel />
-        <SidebarChannel />
-        <SidebarChannel />
-        <SidebarChannel />
-      </div>
+        <div className='sidebar__channelsList'>
+          {channels.map(({ id, channel }) => (
+            <SidebarChannel 
+            key={id} 
+            id={id} 
+            channelName={channel.channelName}
+            />
+          ))}
+        </div>
       </div>
 
-
-      <div className="sidebar__voice">
-        <SignalCellularAltIcon 
-        className="sidebar__voiceIcon"
-        fontSize='large'
+      <div className='sidebar__voice'>
+        <SignalCellularAltIcon
+          className='sidebar__voiceIcon'
+          fontSize='large'
         />
-        <div className="sidebar__voiceInfo">
+        <div className='sidebar__voiceInfo'>
           <h3>Voice Connected</h3>
-          <p>Stream</p> 
+          <p>Stream</p>
         </div>
 
-        <div className="sidebar__voiceIcons">
+        <div className='sidebar__voiceIcons'>
           <InfoOutlinedIcon />
           <CallIcon />
         </div>
       </div>
 
-      <div className="sidebar__profile">
-        <Avatar src='https://media-exp1.licdn.com/dms/image/C5603AQEol2R6uLZy-A/profile-displayphoto-shrink_100_100/0/1622845454299?e=1632355200&v=beta&t=rdeEAlBQMUQmsdBD6ksTsZilUWvihssc8w16J6j3hjA'/>
-        <div className="sidebar__profileInfo">
-          <h3>ShaunPurslow</h3>
-          <p>#thisIsMyId</p>
+      <div className='sidebar__profile'>
+        <Avatar onClick={() => auth.signOut()} src={user.photo} />
+        <div className='sidebar__profileInfo'>
+          <h3>{user.displayName}</h3>
+          <p>#{user.uid.substring(0, 5)}</p>
         </div>
 
-        <div className="sidebar__profileIcons">
+        <div className='sidebar__profileIcons'>
           <MicIcon />
           <HeadsetIcon />
           <SettingsIcon />
--- src/SidebarChannel.js
+++ src/SidebarChannel.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './SidebarChannel.css';
 
-const SidebarChannel = ({ id, channel }) => {
+const SidebarChannel = ({ id, channelName }) => {
   return (
     <div className='sidebarChannel'>
-      <h4><span className='sidebarChannel__hash'>#</span>React</h4>
+      <h4><span className='sidebarChannel__hash'>#</span>
+      {channelName}
+      </h4>
     </div>
   )
 }
