ac9509c4da2881ded6118395d112cfb498b2c38b
Shaun <Shaun@Shauns-MacBook-Pro.local>
2021-07-22 20:56:08 -0600

    make a components folder, move files to each category, setup the functionality

---
+++ src/components/login/Login.js
@@ -0,0 +1,22 @@
+import { Button } from '@material-ui/core';
+import { auth, provider } from '../../firebase';
+import React from 'react';
+import './Login.css';
+
+const Login = () => {
+  const signIn = () => {
+    //do google login
+    auth.signInWithPopup(provider).catch((error) => alert(error.message));
+  }
+  return (
+    <div className='login'>
+      <div className="login__logo">
+        <img src="https://logovtor.com/wp-content/uploads/2020/07/discord-logo-vector.png" alt="Discord Logo" />
+      </div>
+
+      <Button onClick={signIn}>Sign In</Button>
+    </div>
+  )
+}
+
+export default Login
